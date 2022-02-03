8a9a61d90855d4d464879be61a2f2e5efe3feb73
ariellecastro <acastro26@my.bcit.ca>
2022-02-03 15:56:23 -0800

    Started nav bar

---
+++ comps/Nav/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+const NavBar = ({
+    iconColor= "white",
+    fontSize= "18px", 
+    iconSize= "18px",
+    bgColor= "#FFF",
+}) => {
+
+    return<div>
+
+    </div>
+}
+
+export default NavBar;
