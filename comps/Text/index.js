7b6c1ab027435150f321d807ec3483bb6d46b1a2
zoelake <zoe_lake@icloud.com>
2022-02-03 15:56:07 -0800

    text comp example

---
+++ comps/Text/index.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { themes } from "../../utils/variables";
+import { useTheme } from "../../utils/provider";
+
+const Text = styled.p`
+    color: ${props=>props.color};
+    font-size: ${props=>props.fontSize};
+    line-height: auto;
+    margin:15px;
+    padding:0;
+`;
+
+export default function MyText({
+    text = 'button',
+    size = '30px'
+}){
+
+    const {theme} = useTheme();
+   
+
+    return <Text
+    color={themes[theme].focus}
+    fontSize={size}
+    >{text}</Text>
+}
