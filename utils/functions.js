2524c8402b0066a8541109b6576ab3aa4539506b
zoelake <zoe_lake@icloud.com>
2022-02-04 17:39:55 -0800

    clean data + filters done

--- utils/functions.js
+++ utils/functions.js
@@ -4,6 +4,9 @@ const tracks = require('./dataSet.json')
 function filtering(
     arr = [],
     config = {
+        //easy test
+        area:null,
+        //values
         explicit: null,
         liveness: null,
         danceability: null,
@@ -13,7 +16,7 @@ function filtering(
         instrumentals: null,
         acousticness: null,
         valence: null,
-
+        //genres
         country: null,
         danceElectronic: null,
         hipHop: null,
@@ -26,22 +29,96 @@ function filtering(
         pop: null,
         rbSoul: null,
         rap: null,
-        reggaeton: null,
+        reggae: null,
         rock: null,
         trap: null,
     }
 ) {
-    const { explicit, liveness, danceability, energy, loudness, tempo, instrumentals, acousticness, valence, country, danceElectronic, hipHop, house, indie, jazz, kPop, latin, metal, pop, rbSoul, rap, reggaeton, rock, trap } = config;
+    const { area, explicit, liveness, danceability, energy, loudness, tempo, instrumentals, acousticness, valence, country, danceElectronic, hipHop, house, indie, jazz, kPop, latin, metal, pop, rbSoul, rap, reggae, rock, trap } = config;
 
-    if (explicit || liveness || danceability || energy || loudness || tempo || instrumentals || acousticness || valence || country || danceElectronic || hipHop || house || indie || jazz || kPop || latin || metal || pop || rbSoul || rap || reggaeton || rock || trap) {
+    if (area || explicit || liveness || danceability || energy || loudness || tempo || instrumentals || acousticness || valence || country || danceElectronic || hipHop || house || indie || jazz || kPop || latin || metal || pop || rbSoul || rap || reggae || rock || trap) {
         const filtered_arr = arr.filter((o) => {
             var cond = true;
 
-            //put filter conditions here
+            if (area) {
+                cond = cond && (o.Country).includes(area);
+            }
 
+            if(explicit) {
+                cond = cond && (o.Explicit) === explicit;
+            }
+            if(liveness) {
+                cond = cond && (o.liveliness) >= liveness;
+            }
+            if(danceability) {
+                cond = cond && (o.danceability) >= danceability;
+            }
+            if(energy) {
+                cond = cond && (o.energy) >= energy;
+            }
+            if(loudness) {
+                cond = cond && (o.loudness) >= loudness;
+            }
+            if(tempo) {
+                cond = cond && (o.tempo) >= tempo;
+            }
+            if(instrumentals) {
+                cond = cond && (o.instrumentalness) >= instrumentals;
+            }
+            if(acousticness) {
+                cond = cond && (o.acoustics) >= acousticness;
+            }
+            if(valence) {
+                cond = cond && (o.valence) >= valence;
+            }
+            if(country) {
+                cond = cond && (o.country) === 1;
+            }
+            if(danceElectronic) {
+                cond = cond && (o.danceElectronic) === 1;
+            }
+            if(hipHop) {
+                cond = cond && (o.hipHop) === 1;
+            }
+            if(house) {
+                cond = cond && (o.house) === 1;
+            }
+            if(indie) {
+                cond = cond && (o.indie) === 1;
+            }
+            if(jazz) {
+                cond = cond && (o.jazz) === 1;
+            }
+            if(kPop) {
+                cond = cond && (o.hipHop) === 1;
+            }
+            if(latin) {
+                cond = cond && (o.latin) === 1;
+            }
+            if(metal) {
+                cond = cond && (o.metal) === 1;
+            }
+            if(pop) {
+                cond = cond && (o.pop) === 1;
+            }
+            if(rbSoul) {
+                cond = cond && (o.rbSoul) === 1;
+            }
+            if(rap) {
+                cond = cond && (o.rap) === 1;
+            }
+            if(reggae) {
+                cond = cond && (o.reggae) === 1;
+            }
+            if(rock) {
+                cond = cond && (o.rock) === 1;
+            }
+            if(trap) {
+                cond = cond && (o.trap) === 1;
+            }
             return cond;
         })
-        console.log(filtered_arr.slice(0, 5));
+        // console.log(filtered_arr.slice(0, 5));
         return filtered_arr;
     } else {
         console.log([])
@@ -50,47 +127,48 @@ function filtering(
 }
 
 
-// function sorting(
-//     arr = [],
-//     config = { key: null, type: null }
-// ) {
-//     const { key, type } = config;
+function sorting(
+    arr = [],
+    config = { key: null, type: null }
+) {
+    const { key, type } = config;
 
-//     if (key) {
-//         arr.sort((cur, next) => {
-//             var num1 = Number(cur[key]);
-//             var num2 = Number(next[key]);
+    if (key) {
+        arr.sort((cur, next) => {
+            var num1 = Number(cur[key]);
+            var num2 = Number(next[key]);
 
-//             if (isNaN(cur[key])) {
-//                 num1 = cur[key];
-//                 num2 = next[key];
-//             }
+            if (isNaN(cur[key])) {
+                num1 = cur[key];
+                num2 = next[key];
+            }
 
 
-//             if (num1 > num2) {
-//                 if (type && type === 'desc') {
-//                     return 1;
-//                 }
-//                 return -1
-//             }
-//             if (num1 < num2) {
-//                 if (type && type === 'desc') {
-//                     return -1;
-//                 }
-//                 return 1;
-//             }
-//             return 0;
-//         })
+            if (num1 > num2) {
+                if (type && type === 'desc') {
+                    return 1;
+                }
+                return -1
+            }
+            if (num1 < num2) {
+                if (type && type === 'desc') {
+                    return -1;
+                }
+                return 1;
+            }
+            return 0;
+        })
 
-//         console.log(arr.slice(0, 10));
-//         return arr;
-//     }
-// }
+        // console.log(arr.slice(0, 10));
+        return arr;
+    }
+}
 
-var f_books = filtering(tracks, {
-    explicit: false,
-    // liveness: null,
-    // danceability: null,
+var f_tracks = filtering(tracks, {
+    area: "Sweden",
+    // explicit: false,
+    liveness: 0.002,
+    danceability: 0.04,
     // energy: null,
     // loudness: null,
     // tempo: null,
@@ -110,15 +188,18 @@ var f_books = filtering(tracks, {
     // pop: null,
     // rbSoul: null,
     // rap: null,
-    // reggaeton: null,
+    // reggae: null,
     // rock: null,
     // trap: null,
 })
 
-// filtered_tracks = sorting(tracks, {
-//     key: 'explicit',
-//     type: 'asc'
-// });
+f_tracks = sorting(tracks, {
+    key: 'Title',
+    type: 'desc'
+});
 
 
-// console.log(filtered_tracks);
+// console.log("these are your tracks: " + f_tracks.splice(0,5));
+console.log("string tracks 1: " + JSON.stringify(f_tracks[0].Title));
+console.log("string tracks 2: " + JSON.stringify(f_tracks[1].Title));
+
