c66c4322cc62e821a0f110991866743275565c1f
ariellecastro <acastro26@my.bcit.ca>
2022-02-06 22:12:18 -0800

    Editing Nav Bar

--- comps/Nav/index.js
+++ comps/Nav/index.js
@@ -1,16 +1,98 @@
 import styled from "styled-components";
+import { useState, useRef } from "react";
+import { themes } from "../../utils/variables";
+import { useTheme } from "../../utils/provider";
+import { RiMvFill, RiHome2Fill, RiSettings4Fill } from "react-icons/ri";
+import { MyText } from "../Text/index";
+import WebFont from 'webfontloader';
+
+WebFont.load({
+
+    google: {
+        families: ['Be Vietnam Pro:400, 600, 700', 'sans-serif']
+    }
+});
+
+const Container = styled.div`
+    visibility: ${props=>props.visibility};
+    width: 15%;
+    height: 100%;
+    background-color: ${props=>props.bg};
+    display: flex;
+    flex-direction: column;
+    font: 'Arial';
+    
+`;
+
+const NavLink = styled.a`
+    padding: 10px;
+    margin: 10px;
+    maring-top: 20px;
+    background-color: transparent ;
+    border: none;
+    text-align:left;
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 18px;
+    color: ${props=>props.color};
+`;
+
+const LinkCont = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
 
 
 const NavBar = ({
     iconColor= "white",
     fontSize= "18px", 
     iconSize= "18px",
-    bgColor= "#FFF",
+    bgColor= "#212121",
+
 }) => {
 
-    return<div>
+    const [hideNav, setHideNav] = useState(true)
+    const [visibility, setVisibility] = useState()
 
-    </div>
+    function View(){
+        setHideNav(!hideNav)
+        if (hideNav === true){
+        setVisibility("visible")
+        }
+        if (hideNav === false){
+            setVisibility("hidden")
+        }
+    }
+    const {theme} = useTheme();
+
+
+    return <><Container
+        visibility={visibility}
+        bg={themes[theme].mid}
+    >
+        <LinkCont>
+        <NavLink href="/" color={themes[theme].highlight}>
+            <RiHome2Fill/> Home
+        </NavLink>
+        </LinkCont>
+
+        <LinkCont>
+        <NavLink href="/" color={themes[theme].focus}>
+            <RiMvFill/> Library
+        </NavLink>
+        </LinkCont>
+
+        <LinkCont>
+        <NavLink href="/" color={themes[theme].focus}>
+            <RiSettings4Fill/> Settings
+        </NavLink>
+        </LinkCont>
+ 
+    </Container>
+            <button onClick={View}>Toggle</button>
+
+    </>
 }
 
 export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import MyButton from '../comps/Button'
+import NavBar from '../comps/Nav'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
 
       <MyButton />
       
+      <NavBar></NavBar>
       
     </div>
   )
