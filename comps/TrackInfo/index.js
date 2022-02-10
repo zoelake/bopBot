61d5a28a844082550873b672a08f55c09d7e3555
ariellecastro <acastro26@my.bcit.ca>
2022-02-10 15:37:59 -0800

    Done track info

--- comps/TrackInfo/index.js
+++ comps/TrackInfo/index.js
@@ -8,7 +8,8 @@ const Text = styled.p`
     color: ${props=>props.color};
     font-size: ${props=>props.fontSize};
     line-height: auto;
-    margin:15px;
+    margin:0 15px 0 15px;
+    
     padding:0;
 
     :hover{
@@ -21,11 +22,53 @@ const TrackCont = styled.div`
     align-items: center;
     flex-direction: row;
     justify-content: space-evenly
+    border : 1px red;
+    max-width: 800px;
+`;
+
+const Cont1= styled.div`
+    display: flex;
+    border : 1px solid red;
+    flex-grow: 1;
+    height: 82px;
+`;
+
+const Cont2 = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 82px;
+    border : 1px solid red;
+    flex-grow: 2;
+`;
+
+const Cont3 = styled.div`
+    display: flex;
+    border : 1px solid red;
+    height: 82px;
+    flex-grow: 1;
+`;
+
+const Cont4 = styled.div`
+    display: flex;
+    border : 1px solid red;
+    height: 82px;
+    flex-grow: 1;
+`;
+
+const Cont5 = styled.div`
+    display: flex;
+    border : 1px solid red;
+    height: 82px;
+    flex-grow: 1;
 `;
 
 export default function MyTrack({
     text = 'button',
     size = '18px',
+    time = '2:55',
+    artist = 'Zoe James',
+    song = 'In the House',
+    album = 'Diffy',
     onClick = () => {},
 }){
     const [heart, setHeart] = useState(false);
@@ -33,29 +76,47 @@ export default function MyTrack({
    
 
     return <TrackCont>
-        <Text
-        color={themes[theme].focus}
-        >1</Text>
-        <Text
-        color={themes[theme].focus}
-        >In the House
+
+        <Cont1>
+            <Text
+            color={themes[theme].focus}
+            >1</Text>
+        </Cont1>
+
+        <Cont2>
+            <Text color={themes[theme].focus}>
+                {song}
+            </Text>
+
             <Text
             color={themes[theme].highlight}
             textHover={themes[theme].accent1}
-            >Zoe James</Text>
-        </Text>
-        <Text
-        color={themes[theme].focus}
-        >Time</Text>
-        <Text
-        color={themes[theme].focus}
-        >Diffy</Text>
-        <Text
-        color={themes[theme].focus}
-        textHover={themes[theme].accent1}
-        >
-        {heart===false?<RiHeartLine onMouseOver={()=>setHeart(true)}/>:<RiHeartFill onMouseOut={()=>setHeart(false)}/>}
-        </Text>
+            >{artist}</Text>
+        </Cont2>
+
+        <Cont3>
+            <Text
+            color={themes[theme].focus}
+            >{time}</Text>
+        </Cont3>
+
+        <Cont4>
+            <Text
+            color={themes[theme].focus}
+            >{album}</Text>
+        </Cont4>
+
+        <Cont5>
+            <Text
+            color={themes[theme].focus}
+            textHover={themes[theme].accent1}
+            >
+            {heart===false?<RiHeartLine onMouseOver={()=>setHeart(true)}/>:<RiHeartFill onMouseOut={()=>setHeart(false)}/>}
+            </Text>
+        </Cont5>
+
+
+
 
         
     </TrackCont>
